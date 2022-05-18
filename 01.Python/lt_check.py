916d620016d1bc453bec9c4aeb32f85ed0033479
tpa <tp_proton_aly@protonmail.com>
2022-05-18 10:43:10 +0800

    更新获取官方数据对比43

--- 01.Python/lt_check.py
+++ 01.Python/lt_check.py
@@ -3,13 +3,11 @@
 
 import re
 import sys
+import json
+import requests
+import time
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+usr_input_code = ""
 def lottery_code_check(input_code, release_code):
     """
     check code
@@ -64,19 +62,60 @@ def lottery_code_check(input_code, release_code):
         return [9, 5]
     return [0, 0]
 
+def get_lottery_info_from_office():
+    url = "https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry?gameNo=85&provinceId=0&pageSize=30&isVerify=1&pageNo=1&termLimits=30"
+    try:
+        r = requests.get(url)
+        dict = json.loads(r.text)
+        data_list = dict["value"]["list"]
+        # data_len  = len(data_list)
+        # for index in range(data_len):
+        #     print(f'{data_list[index]["lotteryDrawTime"]}:{data_list[index]["lotteryDrawResult"]}')
+        for data in data_list:
+            # print(f'{data["lotteryDrawTime"]}:{data["lotteryDrawResult"]}')
+            tp_list = list(data["lotteryDrawResult"])
+            tp_list.insert(-6, " ")
+            tp_list.insert(-6, "+")
+            # print(tp_list)
+            office_code = ''.join(tp_list)
+            # global usr_input_code
+            # usr_input_code = "11 14 22 23 27 + 08 10"
+            result_list = lottery_code_check(usr_input_code, office_code)
+            if result_list[0] != 0:
+                rs = f'{data["lotteryDrawTime"]} --> {data["lotteryDrawResult"]} --> result:{result_list}'
+                print(rs)
+                write_exec_result_to_file(rs)
+
+    except Exception as ex:
+        print(f"Error {ex}")
+
+    else:
+        print("ok")
+
+def write_exec_result_to_file(str):
+    out_file_name = "exec_result.html"
+    file = open(out_file_name, mode="a+", encoding="UTF-8")
+    input = f"<br>{str}"
+    file.write(input)
+
+def write_time_info():
+    time_stamp = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
+    tp_str = f"the python exec at {time_stamp}"
+    write_exec_result_to_file(tp_str)
+
 
 if __name__ == '__main__':
-    print(sys.argv[0])
-    level = lottery_code_check("10 15 19 28 32 + 04 09", "10 15 19 28 32 + 04 09")
-    if level == 1:
-        print("big price")
-    else:
-        print(level)
+    write_time_info()
+    # level = lottery_code_check("10 15 19 28 32 + 04 09", "10 15 19 28 32 + 04 09")
     if len(sys.argv) >= 2:
-        print(f"参数为:{sys.argv[1]}")
-        out_file_name = "123456.txt"
-        file = open(out_file_name, mode="w", encoding="UTF-8")
-        input = f"第二个参数是:{sys.argv[1]} \n<br> 第三个参数:{sys.argv[2]} \n<br> 第四个参数:{sys.argv[3]}"
-        file.write(input)
+        usr_input_code = sys.argv[1]
+        # print(f"参数为:{sys.argv[1]}")
+        # out_file_name = "exec_result.html"
+        # file = open(out_file_name, mode="w", encoding="UTF-8")
+        # input = f"第二个参数是:{sys.argv[1]} \n<br> 第三个参数:{sys.argv[2]} \n<br> 第四个参数:{sys.argv[3]}"
+        # file.write(input)
+        # print(f"usr_input_code:{usr_input_code}")
+
+    get_lottery_info_from_office()
 
 
