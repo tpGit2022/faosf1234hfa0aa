a2252d4bdf08df807f4bab712bc64128cc5efbdf
tpa <tp_proton_aly@protonmail.com>
2022-05-21 09:52:22 +0800

    update python script

--- 01.Python/lt_check.py
+++ 01.Python/lt_check.py
@@ -2,15 +2,24 @@
 # _*_ coding:UTF-8 _*_
 
 import json
+import os
 import re
+import smtplib
 import sys
 import time
+from email.header import Header
+from email.mime.text import MIMEText
 
 import requests
+
 # avoid ssl error
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL:@SECLEVEL=1'
 
 usr_input_code = ""
+need_send_email = False
+report_file_name = 'exec_result.html'
+
+
 def lottery_code_check(input_code, release_code):
     """
     check code
@@ -65,10 +74,12 @@ def lottery_code_check(input_code, release_code):
         return [9, 5]
     return [0, 0]
 
+
 def get_lottery_info_from_office():
     url = "https://www.gdlottery.cn/gdata/idx/tcnotice"
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/98.0.4758.102 Safari/537.36",
         "DNT": "1",
         "X-Requested-With": "XMLHttpRequest",
         "sec-ch-ua-mobile": "?0",
@@ -78,9 +89,8 @@ def get_lottery_info_from_office():
         "Sec-Fetch-Dest": "empty",
         "Referer": "https://www.gdlottery.cn/?v=1652849733245"
     }
-    r = requests.get(url, headers= headers)
-    print(r.text)
-    # write_exec_result_to_file(r.text)
+    r = requests.get(url, headers=headers)
+    # print(r.text)
     lt_list = json.loads(r.text)
     origin_code = lt_list[0]["kjhm"]
     print(f"origin_code={origin_code}")
@@ -88,63 +98,80 @@ def get_lottery_info_from_office():
     origin_code = origin_code.replace("+", " ")
     origin_code = origin_code.replace("@", "+")
     ret_list = lottery_code_check(usr_input_code, origin_code)
-    if ret_list[0] !=0:
+    if ret_list[0] != 0:
         print(ret_list)
+        global need_send_email
+        need_send_email = True
         tp_str = f"<br>Congratulate you are so lucky {ret_list}<br>"
         write_exec_result_to_file(tp_str)
     else:
         tp_str = f"<br>nothing hit...<br>"
         write_exec_result_to_file(tp_str)
-
-    # data_list = dict["value"]["list"]
-    #     # data_len  = len(data_list)
-    #     # for index in range(data_len):
-    #     #     print(f'{data_list[index]["lotteryDrawTime"]}:{data_list[index]["lotteryDrawResult"]}')
-    # for data in data_list:
-    #         # print(f'{data["lotteryDrawTime"]}:{data["lotteryDrawResult"]}')
-    #     tp_list = list(data["lotteryDrawResult"])
-    #     tp_list.insert(-6, " ")
-    #     tp_list.insert(-6, "+")
-    #         # print(tp_list)
-    #     office_code = ''.join(tp_list)
-    #         # global usr_input_code
-    #         # usr_input_code = "11 14 22 23 27 + 08 10"
-    #     result_list = lottery_code_check(usr_input_code, office_code)
-    #     if result_list[0] != 0:
-    #         rs = f'{data["lotteryDrawTime"]} --> {data["lotteryDrawResult"]} --> result:{result_list}'
-    #         print(rs)
-    #         write_exec_result_to_file(rs)
-
-    # except Exception as ex:
-    # error_msg = f"<br> python script exec exception see the info:<br>${ex}"
-    # write_exec_result_to_file(error_msg)
-    # else:
     success_msg = f"<br>python exec as expect"
     write_exec_result_to_file(success_msg)
 
-def write_exec_result_to_file(str):
-    out_file_name = "exec_result.html"
-    file = open(out_file_name, mode="a+", encoding="UTF-8")
-    input = f"<br>{str}"
-    file.write(input)
 
-def write_time_info():
-    time_stamp = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
-    tp_str = f"the python exec at {time_stamp}"
+def write_exec_result_to_file(log_str):
+    file = open(report_file_name, mode="a+", encoding="UTF-8")
+    input_str = f"<br>{log_str}"
+    file.write(input_str)
+
+
+def write_message_header():
+    m_time_stamp = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
+    tp_str = f"the python exec at {m_time_stamp}"
+    r = requests.get("https://ip.gs/json")
+    json_str = json.loads(r.text)
+    print(json_str)
+
+    tp_str = tp_str + f"<br>IP:{json_str['ip']}<br>Country:{json_str['country']}<br>" \
+                      f"Region:{json_str.get('region_name', None)}" \
+                      f"<br>City:{json_str.get('city', None)}<br>Latitude:{json_str.get('latitude', None)}\t" \
+                      f"longitude:{json_str.get('longitude', None)}" \
+                      f"<br>TimeZone:{json_str['time_zone']}<br>asnOrg:{json_str.get('asn_org', None)}"
     write_exec_result_to_file(tp_str)
 
 
+def send_email_with_smtp():
+    # if not need_send_email:
+    #     print("Github Action Python Script, Do not trigger Send Email Action")
+    #     return
+    # py3_env = os.environ['PYTHON3HOME']
+    email_config_server_domain = os.environ['EMAIL_SMTP_DOMAIN']
+    email_config_server_port = os.environ['EMAIL_SMTP_PORT']
+    email_config_server_user_name = os.environ['EMAIL_SMTP_USER_NAME']
+    email_config_server_user_pwd = os.environ['EMAIL_SMTP_USER_PWD']
+    email_config_server_recv_user_name = os.environ['EMAIL_SMTP_REV']
+
+    with open(report_file_name, 'r') as report_f:
+        report_string = report_f.read()
+    msg = MIMEText(report_string, 'plain', 'UTF-8')
+    msg['From'] = f"{email_config_server_user_name}"
+    msg['To'] = f"{email_config_server_recv_user_name}"
+    msg['Subject'] = Header("Python LT Script Check Result", 'UTF-8').encode()
+    # msg['From'] = formataddr(['smtp_python_user_name', f"{email_config_server_user_name}")
+    # msg['To'] = Header(email_config_server_recv_user_name, 'UTF-8')
+    # msg['Subject'] = Header('test python email module', 'UTF-8').encode()
+    #
+    smtp_obj = smtplib.SMTP_SSL(email_config_server_domain, int(email_config_server_port))
+
+    smtp_obj.set_debuglevel(1)
+    smtp_obj.login(user=email_config_server_user_pwd, password=email_config_server_user_pwd)
+    smtp_obj.sendmail('tp_net_cloud@163.com', [email_config_server_recv_user_name], msg.as_string())
+    smtp_obj.quit()
+
+
 if __name__ == '__main__':
-    write_time_info()
-    # level = lottery_code_check("10 15 19 28 32 + 04 09", "10 15 19 28 32 + 04 09")
-    if len(sys.argv) >= 2:
-        usr_input_code = sys.argv[1]
-        # print(f"参数为:{sys.argv[1]}")
-        # out_file_name = "exec_result.html"
-        # file = open(out_file_name, mode="w", encoding="UTF-8")
-        # input = f"第二个参数是:{sys.argv[1]} \n<br> 第三个参数:{sys.argv[2]} \n<br> 第四个参数:{sys.argv[3]}"
-        # file.write(input)
-        # print(f"usr_input_code:{usr_input_code}")
-    get_lottery_info_from_office()
-
-
+    write_message_header()
+    try:
+        if len(sys.argv) >= 2:
+            usr_input_code = sys.argv[1]
+        if len(sys.argv) >= 5:
+            time_stamp = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
+            start_time = sys.argv[4]
+            term_period = sys.argv[3]
+        get_lottery_info_from_office()
+    except KeyError:
+        need_send_email = True
+    need_send_email = True
+    send_email_with_smtp()
