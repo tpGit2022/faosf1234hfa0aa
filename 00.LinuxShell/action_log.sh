6de3fe292720a17096b22c951b0fc503555b706f
tpa <tp_proton_aly@protonmail.com>
2022-06-01 13:49:59 +0800

    Linux shell打印信息添加说明

--- 00.LinuxShell/action_log.sh
+++ 00.LinuxShell/action_log.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 currentTimeStamp=`date "+%Y_%m_%d_%H_%M_%S"`
 #rm -rf email_content.txt
-echo $currentTimeStamp
-echo $(TZ=UTC-8 date +%Y_%m_%d_%H_%M_%S)
+echo 当前环境时间:$currentTimeStamp
+echo 当前北京时间:$(TZ=UTC-8 date +%Y_%m_%d_%H_%M_%S)
 echo $(TZ=UTC-8 date +%Y_%m_%d_%H_%M_%S) >> exec_result.html
