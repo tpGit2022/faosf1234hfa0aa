abaa92ed7ab61cc6d3d3219c56a2dc3cb2b6d20e
tpa <tp_proton_aly@protonmail.com>
2022-06-02 10:45:40 +0800

    test1045

--- 01.Python/test.py
+++ 01.Python/test.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding:UTF-8 -*-
-import sys
+import os
 
-import requests
-import time
 
-def checkNum(hit, input):
-    r = requests.get("https://mirrors.tuna.tsinghua.edu.cn/AdoptOpenJDK/8/jdk/x64/windows/")
-    if r.status_code == 200:
-        # print(r.encoding)
-        time_ticket = time.strftime("%Y_%m_%d", time.localtime())
-        str_format = "{timestamp:s}.html"
-        out_file_name = str_format.format(timestamp=time_ticket)
-        print(out_file_name)
-        file = open(out_file_name, mode="w", encoding="ISO-8859-1")
-        file.write(r.text)
+def checkNum():
+    print(os.environ['ENV_TEST'])
 
 
 if __name__ == "__main__":
