00c440655b2a81782d807691ac63e9d5045ee264
tpa <tp_proton_aly@protonmail.com>
2022-06-06 08:11:07 +0800

    新增网易云PC端签到代码

--- 01.Python/CheckIn.py
+++ 01.Python/CheckIn.py
@@ -1,5 +1,5 @@
-#! /usr/bin/python3
-# _*_ coding:UTF-8 _*_
+#!/usr/bin/env python3
+# -*- coding:UTF-8 -*-
 import os
 import requests
 import json
@@ -10,6 +10,7 @@ def do_v2ray_check_in():
     v2ray 服务器签到获取流量
     :return:
     """
+    print("执行V2ray账号签到获取流量....")
     login_url = "https://ssru6.pw/auth/login"
     headers = {
         "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
@@ -67,5 +68,34 @@ def do_v2ray_check_in():
         print(f"签到失败!!!\r\n{check_in_json_data}")
 
 
-if __name__ == '__main__':
+def netease_cloud_pc_sign_in():
+    print("执行网易云音乐PC端签到....")
+    str_cookies = os.environ['NETEASE_CLOUD_MUSIC_PC_SIGN_COOKIES']
+    str_cookie_array = str_cookies[7:].split(';')
+    index = 0
+    dict_netease_cloud_music_cookie = {}
+    while index < len(str_cookie_array):
+        str_cookie = str_cookie_array[index].split("=")
+        # print(f"key={str_cookie[0].strip()} value={str_cookie[1].strip()}")
+        dict_netease_cloud_music_cookie[str_cookie[0].strip()] = str_cookie[1].strip()
+        index = index + 1
+    post_params = os.environ['NETEASE_CLOUD_MUSIC_PC_SIGN_PARAMS']
+    # print(f"cookies:{dict_netease_cloud_music_cookie}")
+    # print("=============================================")
+    # print(post_params)
+    url = "http://interface.music.163.com/eapi/point/dailyTask"
+    headers = {
+                  "Accept": "*/*",
+                  "Content-Type": "application/x-www-form-urlencoded",
+                  "Origin": "orpheus://orpheus",
+                  "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome / 35.0.1916.157 Safari / 537.36",
+                  "Accept-Encoding": "gzip,deflate",
+                  "Accept - Language": "en - us, en;q = 0.8"
+    }
+    r = requests.post(url, headers=headers, cookies=dict_netease_cloud_music_cookie,data=post_params)
+    print(r.text)
+
+
+if __name__ == "__main__":
     do_v2ray_check_in()
+    netease_cloud_pc_sign_in()
