33759586036511da2069d6f0095c8669874b3e18
tpa <tp_proton_aly@protonmail.com>
2022-06-20 08:02:40 +0800

    update print info 0802

--- 01.Python/lt_check.py
+++ 01.Python/lt_check.py
@@ -116,8 +116,7 @@ def get_lottery_info_from_office(end_period_num):
         tp_str = f"<br>nothing hit...<br>"
         write_exec_result_to_file(tp_str)
     success_msg = f"the cur period num:{current_period_num} date:{current_date}<br>the end period num:{end_period_num}" \
-                  f"<br>usr_input_code:{usr_input_code}<br>office_re_code:{origin_code}<br" \ 
-                  f">python exec as expect "
+                  f"<br>usr_input_code:{usr_input_code}<br>office_re_code:{origin_code}<br>python exec as expect"
     write_exec_result_to_file(success_msg)
     # print office release code
     lt_index = 0
