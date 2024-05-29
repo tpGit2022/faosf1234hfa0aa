fc9f44b4a87c87ca48c6528898b74f1178bd345c
Traveler <102571186+tpGit2022@users.noreply.github.com>
2024-05-29 08:38:41 +0800

    Update action_log.sh

--- 00.LinuxShell/action_log.sh
+++ 00.LinuxShell/action_log.sh
@@ -3,7 +3,7 @@ currentTimeStamp=`date "+%Y_%m_%d_%H_%M_%S"`
 #rm -rf email_content.txt
 echo 当前环境时间:$currentTimeStamp
 echo 当前北京时间:$(TZ=UTC-8 date +%Y_%m_%d_%H_%M_%S)
-echo 当前北京时间:$(TZ=UTC-8 date +%Y_%m_%d_%H_%M_%S) >> exec_result.html
+echo 当前北京时间:$(TZ=UTC-8 date +%Y_%m_%d_%H_%M_%S) > exec_result.html
 cat exec_result.html
 curl ip.gs
 #ls -alR ..
